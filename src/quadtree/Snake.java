3add0ae3c6c82f80060dda67ae6672e1d84cb587
Douzanee <66843732+Douzanee@users.noreply.github.com>
2021-05-15 18:06:59 -0300

    Projeto de Snake em Java.

---
+++ src/quadtree/Snake.java
@@ -0,0 +1,34 @@
+package quadtree;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+	
+public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Just a simple snake game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+
+}
